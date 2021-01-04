035d1bed5f4e3fa68b4626f273fb8f2749fe7976
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-01-04 16:57:31 +0100

    can now add and delete categories

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -23,9 +23,32 @@ router.post(`/`, async(req, res) => {
   category = await category.save()
 
   if (!category) {
-    return res.status(404).send('Category cannot be created')
+    return res.status(404).send('Category cannot be created!')
   }
   res.send(category)
+})
+
+router.delete(`/:id`, (req, res) => {
+  Category.findByIdAndRemove(req.params.id)
+  .then(category => {
+    if (category) {
+      return res.status(200).json({
+        success: true,
+        message: 'Category deleted successfully!'
+      })
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: 'Category could not be deleted!'
+      })
+    }
+  })
+  .catch(err => {
+    return res.status(400).json({
+      success: false,
+      error: err
+    })
+  })
 })
 
 module.exports = router
