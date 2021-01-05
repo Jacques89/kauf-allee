249029119094ca8f6765eed0da8518ea4e5b207e
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-01-05 13:56:37 +0100

    Product can now be updated in restAPI

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -43,13 +43,13 @@ router.put(`/:id`, async(req, res) => {
     req.params.id,
     {
       name: req.body.name,
-      icon: req.body.icon,
+      icon: req.body.icon || category.icon,
       color: req.body.color,
     },
     { new: true }
   )
   if (!category) {
-    return res.status(404).send('Category cannot be created!')
+    return res.status(404).send('Category cannot be updated!')
   }
   res.send(category)
 })
--- server/routes/products.js
+++ server/routes/products.js
@@ -54,4 +54,34 @@ router.post(`/`, async(req, res) => {
   res.send(product)
 })
 
+router.put(`/:id`, async(req, res) => {
+  const category = await Category.findById(req.body.category)
+
+  if (!category) {
+    return res.status(400).send('Invalid Category')
+  }
+
+  const product = await Product.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      description: req.body.description,
+      mainDescription: req.body.mainDescription,
+      image: req.body.image,
+      brand: req.body.brand,
+      price: req.body.price,
+      category: req.body.category,
+      stockCount: req.body.stockCount,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+      isFeatured: req.body.isFeatured,
+    },
+    { new: true }
+  )
+  if (!product) {
+    return res.status(500).send('Product cannot be updated!')
+  }
+  res.send(product)
+})
+
 module.exports = router
