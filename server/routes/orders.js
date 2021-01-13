1090f89fc03936c24caae2dd0425064b45f8dd27
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-01-13 17:00:10 +0100

    can now delete and update orders

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -17,7 +17,7 @@ const { OrderItem } = require('../models/order-item')
  * @apiDescription Get the Order information
  * @apiGroup Orders
  * @apiPermission admin
- * @apiSuccess {String[]} orderItems Array of Order items the user ordered
+ * @apiSuccess {Object[]} orderItems List of items the User ordered
  * @apiSuccess {String} status The status of the Order
  * @apiSuccess {String} shippingAddress1 The first line of the shipping address
  * @apiSuccess {String} shippingAddress2 The second line of the shipping address
@@ -25,12 +25,13 @@ const { OrderItem } = require('../models/order-item')
  * @apiSuccess {String} postcode The postcode provided by the User
  * @apiSuccess {String} country The country provided by the User
  * @apiSuccess {String} phone The phone number provided by the User
- * @apiSuccess {String} user The id of the User that made the Order
- * @apiSuccess {Date} dateCreated The date the Order was placed
+ * @apiSuccess {Object} user The id of the User that made the Order
+ * @apiSuccess {Date} dateOrdered The date the Order was placed
+ * @apiSuccess {String} id The Order Id
  * @apiError NoAccessRights (401) User is not authorized
  */
 router.get(`/`, async(req, res) => {
-  const orderList = await Order.find()
+  const orderList = await Order.find().populate('user', 'name').sort({'dateOrdered': -1})
 
   if (!orderList) {
     res.status(500).json({
@@ -40,14 +41,14 @@ router.get(`/`, async(req, res) => {
   res.send(orderList)
 })
 
-// POST REQUESTS
 /**
- * @apiName PostOrders
- * @api {post} /orders
- * @apiDescription Create a new Order
+ * @apiName GetOrderId
+ * @api {get} /orders/:id
+ * @apiDescription Get the Order Id
+ * @apiParams {String} id The Order Id
  * @apiGroup Orders
  * @apiPermission admin
- * @apiSuccess {Obj[]} orderItems Array of Order items the user ordered
+ * @apiSuccess {Object[]} orderItems List of items the User ordered
  * @apiSuccess {String} status The status of the Order
  * @apiSuccess {String} shippingAddress1 The first line of the shipping address
  * @apiSuccess {String} shippingAddress2 The second line of the shipping address
@@ -55,8 +56,49 @@ router.get(`/`, async(req, res) => {
  * @apiSuccess {String} postcode The postcode provided by the User
  * @apiSuccess {String} country The country provided by the User
  * @apiSuccess {String} phone The phone number provided by the User
- * @apiSuccess {String} user The id of the User that made the Order
- * @apiSuccess {Date} dateCreated The date the Order was placed
+ * @apiSuccess {Object} user The id of the User that made the Order
+ * @apiSuccess {Date} dateOrdered The date the Order was placed
+ * @apiSuccess {String} id The Order Id
+ * @apiError NoAccessRights 401 User is not authorized
+ */
+router.get(`/:id`, async(req, res) => {
+  const order = await Order
+  .findById(req.params.id)
+  .populate('user', 'name')
+  .populate({ 
+    path: 'orderItems', 
+    populate: {
+      path: 'product',
+      populate: 'category'
+    } 
+  })
+
+  if (!order) {
+    res.status(500).json({
+      success: false
+    })
+  }
+  res.send(order)
+})
+
+// POST REQUESTS
+/**
+ * @apiName PostOrders
+ * @api {post} /orders
+ * @apiDescription Create a new Order
+ * @apiGroup Orders
+ * @apiPermission admin
+ * @apiSuccess {Object[]} orderItems Array of Order items the user ordered
+ * @apiSuccess {String} status The status of the Order
+ * @apiSuccess {String} shippingAddress1 The first line of the shipping address
+ * @apiSuccess {String} shippingAddress2 The second line of the shipping address
+ * @apiSuccess {String} city The city provided by the User
+ * @apiSuccess {String} postcode The postcode provided by the User
+ * @apiSuccess {String} country The country provided by the User
+ * @apiSuccess {String} phone The phone number provided by the User
+ * @apiSuccess {Object} user The id of the User that made the Order
+ * @apiSuccess {Date} dateOrderedThe date the Order was placed
+ * @apiSuccess {String} id The Order Id
  */
 router.post(`/`, async(req, res) => {
   const orderItemsIds = Promise.all(req.body.orderItems.map(async orderItem => {
@@ -88,6 +130,77 @@ router.post(`/`, async(req, res) => {
     return res.status(400).send('Order cannot be created!')
   }
   res.send(order)
+})
+
+// PUT REQUESTS
+/** 
+ * @apiName UpdateOrder
+ * @api {get} /orders/:id
+ * @apiDescription Update an existing Order
+ * @apiParams {String} id The Order Id
+ * @apiGroup Orders
+ * @apiPermission admin
+ * @apiSuccess {Object[]} orderItems List of items the User ordered
+ * @apiSuccess {String} status The status of the Order
+ * @apiSuccess {String} shippingAddress1 The first line of the shipping address
+ * @apiSuccess {String} shippingAddress2 The second line of the shipping address
+ * @apiSuccess {String} city The city provided by the User
+ * @apiSuccess {String} postcode The postcode provided by the User
+ * @apiSuccess {String} country The country provided by the User
+ * @apiSuccess {String} phone The phone number provided by the User
+ * @apiSuccess {Object} user The id of the User that made the Order
+ * @apiSuccess {Date} dateOrdered The date the Order was placed
+ * @apiSuccess {String} id The Order Id
+ * @apiError NoAccessRights (401) User is not authorized
+ */
+router.put(`/:id`, async(req, res) => {
+  const order = await Order.findByIdAndUpdate(
+    req.params.id,
+    {
+      status: req.body.status
+    },
+    { new: true }
+  )
+  if (!order) {
+    return res.status(404).send('Order cannot be updated!')
+  }
+  res.send(order)
+})
+
+// DELETE REQUESTS
+/**
+ * @apiName DeleteOrder
+ * @api {delete} /orders/:id
+ * @apiDescription Delete an existing Order
+ * @apiParam {String} id String value of the category ID
+ * @apiGroup Orders
+ * @apiPermission admin
+ * @apiError OrderNotDeleted (404) The Order could not be deleted
+ */
+router.delete(`/:id`, (req, res) => {
+  Order.findByIdAndRemove(req.params.id)
+  .then(async order => {
+    if (order) {
+      await order.orderItems.map(async orderItem => {
+        await OrderItem.findByIdAndRemove(orderItem)
+      })
+      return res.status(200).json({
+        success: true,
+        message: 'Order deleted successfully!'
+      })
+    } else {
+      return res.status(404).json({
+        success: false,
+        message: 'Order could not be deleted!'
+      })
+    }
+  })
+  .catch(err => {
+    return res.status(400).json({
+      success: false,
+      error: err
+    })
+  })
 })
 
 module.exports = router
