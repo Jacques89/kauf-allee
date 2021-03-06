3d0cc9533b8e97a4390b67dbdac82e17e8c3d3e0
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-03-06 18:45:48 +0100

    users can now delete single items in the cart

--- Screens/Cart/Cart.js
+++ Screens/Cart/Cart.js
@@ -16,9 +16,11 @@ import {
   Thumbnail,
   Body,
 } from 'native-base'
-import Icon from 'react-native-vector-icons/FontAwesome'
-
 import { connect } from 'react-redux'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { SwipeListView } from 'react-native-swipe-list-view'
+
+import CartItem from './CartItem'
 import * as actions from '../../Redux/Actions/cartActions'
 
 const { height, width } = Dimensions.get('window')
@@ -28,38 +30,35 @@ const Cart = (props) => {
   props.cartItems.forEach((cart) => {
     return (total += cart.product.price)
   })
-
   return (
     <>
       {props.cartItems.length ? (
         <Container>
-          <H1 style={{ alignSelf: 'center' }}>Cart</H1>
-          {props.cartItems.map((item, key) => {
-            return (
-              <ListItem style={styles.listItem} key={Math.random()} avatar>
-                <Left>
-                  <Thumbnail
-                    source={{
-                      uri: item.product.image
-                        ? item.product.image
-                        : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png',
-                    }}
-                  />
-                </Left>
-                <Body style={styles.body}>
-                  <Left>
-                    <Text>{item.product.name}</Text>
-                  </Left>
-                  <Right>
-                    <Text>€{item.product.price}</Text>
-                  </Right>
-                </Body>
-              </ListItem>
-            )
-          })}
+          {/* <H1 style={{ alignSelf: "center" }}>Cart</H1> */}
+          <SwipeListView
+            data={props.cartItems}
+            renderItem={(data) => <CartItem item={data} />}
+            renderHiddenItem={(data) => (
+              <View style={styles.hiddenContainer}>
+                <TouchableOpacity
+                  style={styles.hiddenButton}
+                  onPress={() => props.removeFromCart(data.item)}
+                >
+                  <Icon name="trash" color={'white'} size={30} />
+                </TouchableOpacity>
+              </View>
+            )}
+            disableRightSwipe={true}
+            previewOpenDelay={3000}
+            friction={1000}
+            tension={40}
+            leftOpenValue={75}
+            stopLeftSwipe={75}
+            rightOpenValue={-75}
+          />
           <View style={styles.bottomContainer}>
             <Left>
-              <Text style={styles.price}>€ {total}</Text>
+              <Text style={styles.price}>€{total}</Text>
             </Left>
             <Right>
               <Button title="Clear" onPress={() => props.clearCart()} />
@@ -74,8 +73,8 @@ const Cart = (props) => {
         </Container>
       ) : (
         <Container style={styles.emptyContainer}>
-          <Text>Cart is Empty</Text>
-          <Text>Add Products to your cart to get started</Text>
+          <Text>Looks like your cart is empty</Text>
+          <Text>Add products to your cart to get started</Text>
         </Container>
       )}
     </>
@@ -89,9 +88,11 @@ const mapStateToProps = (state) => {
   }
 }
 
+// clear cart
 const mapDispatchToProps = (dispatch) => {
   return {
     clearCart: () => dispatch(actions.clearCart()),
+    removeFromCart: (item) => dispatch(actions.removeFromCart(item)),
   }
 }
 
@@ -100,16 +101,6 @@ const styles = StyleSheet.create({
     height: height,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  listItem: {
-    alignItems: 'center',
-    backgroundColor: 'white',
-    justifyContent: 'center',
-  },
-  body: {
-    margin: 10,
-    alignItems: 'center',
-    flexDirection: 'row',
   },
   bottomContainer: {
     flexDirection: 'row',
@@ -123,6 +114,19 @@ const styles = StyleSheet.create({
     fontSize: 18,
     margin: 20,
     color: 'red',
+  },
+  hiddenContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+  },
+  hiddenButton: {
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    paddingRight: 25,
+    height: 70,
+    width: width / 1.2,
   },
 })
 
