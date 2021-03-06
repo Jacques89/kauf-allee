a128bbe4bd28290b9f6efa749601577649fbb367
Jacques Nalletamby <jacquesnalletamby@gmail.com>
2021-03-06 17:43:28 +0100

    cart UI made and functioning correctly

--- Screens/Cart/Cart.js
+++ Screens/Cart/Cart.js
@@ -1,15 +1,65 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import {
+  View,
+  Dimensions,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+} from 'react-native'
+import {
+  Container,
+  Text,
+  Left,
+  Right,
+  H1,
+  ListItem,
+  Thumbnail,
+  Body,
+} from 'native-base'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 import { connect } from 'react-redux'
+import * as actions from '../../Redux/Actions/cartActions'
+
+const { height, width } = Dimensions.get('window')
 
 const Cart = (props) => {
   return (
-    <View style={{ flex: 1 }}>
-      {props.cartItems.map((item, key) => {
-        return <Text key={item.product._id}>{item.product.name}</Text>
-      })}
-    </View>
+    <>
+      {props.cartItems.length ? (
+        <Container>
+          <H1 style={{ alignSelf: 'center' }}>Cart</H1>
+          {props.cartItems.map((item, key) => {
+            return (
+              <ListItem style={styles.listItem} key={Math.random()} avatar>
+                <Left>
+                  <Thumbnail
+                    source={{
+                      uri: item.product.image
+                        ? item.product.image
+                        : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png',
+                    }}
+                  />
+                </Left>
+                <Body style={styles.body}>
+                    <Left>
+                      <Text>{item.product.name}</Text>
+                    </Left>
+                    <Right>
+                      <Text>€{item.product.price}</Text>
+                    </Right>
+                </Body>
+              </ListItem>
+            )
+          })}
+        </Container>
+      ) : (
+        <Container style={styles.emptyContainer}>
+          <Text>Cart is Empty</Text>
+          <Text>Add Products to your cart to get started</Text>
+        </Container>
+      )}
+    </>
   )
 }
 
@@ -19,5 +69,23 @@ const mapStateToProps = (state) => {
     cartItems: cartItems,
   }
 }
+
+const styles = StyleSheet.create({
+  emptyContainer: {
+    height: height,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listItem: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  body: {
+    margin: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+  }
+})
 
 export default connect(mapStateToProps, null)(Cart)
--- Screens/Products/SingleProduct.js
+++ Screens/Products/SingleProduct.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react'
 import { Image, View, StyleSheet, Text, ScrollView, Button } from 'react-native'
 import { Left, Right, Container, H1 } from 'native-base'
 
+import { connect } from 'react-redux'
+import * as actions from '../../Redux/Actions/cartActions'
+
 const SingleProduct = (props) => {
   const [item, setItem] = useState(props.route.params.item)
   const [availability, setAvailability] = useState(null)
@@ -16,14 +19,12 @@ const SingleProduct = (props) => {
                 ? item.image
                 : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png',
             }}
-            resizeMode='contain'
+            resizeMode="contain"
             style={styles.image}
           />
         </View>
         <View style={styles.contentContainer}>
-          <H1 style={styles.contentHeader}>
-            {item.name}
-          </H1>
+          <H1 style={styles.contentHeader}>{item.name}</H1>
           <Text style={styles.contentText}>{item.brand}</Text>
         </View>
         {/* TODO Description, main description and Availability */}
@@ -34,11 +35,18 @@ const SingleProduct = (props) => {
           <Text style={styles.price}>€{item.price}</Text>
         </Left>
         <Right>
-          <Button title='Add' />
+          <Button title="Add" onPress={() => props.addItemToCart(item)} />
         </Right>
       </View>
     </Container>
   )
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addItemToCart: (product) =>
+      dispatch(actions.addToCart({ quantity: 1, product })),
+  }
 }
 
 const styles = StyleSheet.create({
@@ -53,34 +61,34 @@ const styles = StyleSheet.create({
   },
   image: {
     width: '100%',
-    height: 250
+    height: 250,
   },
   contentContainer: {
     marginTop: 20,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   contentHeader: {
     fontWeight: 'bold',
-    marginBottom: 20
+    marginBottom: 20,
   },
   contentText: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginBottom: 20
+    marginBottom: 20,
   },
   bottomContainer: {
     flexDirection: 'row',
     position: 'absolute',
     bottom: 0,
     left: 0,
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
   price: {
     fontSize: 24,
     margin: 20,
-    color: 'red'
-  }
+    color: 'red',
+  },
 })
 
-export default SingleProduct
+export default connect(null, mapDispatchToProps)(SingleProduct)
